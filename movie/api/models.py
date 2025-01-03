4e18700f8f7635306c7297e9ce6f4916f87dd63e
lagrandecode <seunogunmolufirst1@gmail.com>
2025-01-03 16:16:16 -0500

    "commit"

--- movie/api/models.py
+++ movie/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="/image")
+    director = models.CharField(max_length=50)
+    rating =models.CharField(max_length=10)
+    duration = models.CharField(max_length=10)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    
