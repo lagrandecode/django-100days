24eeb7d7b0e0346224d318191217f16042bec0fe
lagrandecode <seunogunmolufirst1@gmail.com>
2025-01-03 23:35:03 -0500

    "commit"

--- movie/api/urls.py
+++ movie/api/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls import static
 from .views import MovieView
+from django.conf.urls.static import static
 urlpatterns = [
     path('',MovieView.as_view(),name='movie'),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- movie/api/views.py
+++ movie/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Movie
-from .serializer import MovieSerializer
+from .serializers import MovieSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import APIView
 from rest_framework import status
@@ -9,6 +9,13 @@ from rest_framework import status
 
 class MovieView(APIView):
     def get(self, request, format = None):
-        data = Movie.objects.all()
-        serializer = MovieSerializer(data=data)
+        movie = Movie.objects.all()
+        serializer = MovieSerializer(movie,many=True)
         return Response(serializer.data)
+    def post(self, request,format = None):
+        movie = Movie.objects.all()
+        serializer = MovieSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
